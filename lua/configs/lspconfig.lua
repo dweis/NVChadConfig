82f8bc302cff1d41e8c6100b21cf7e1f8d884938
Derrick Weis <derrick@derrickweis.com>
2024-06-15 21:50:37 -0700

    better configuration for rust-analyzer, add trouble.nvim

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "clangd", "metals", "rust_analyzer", "tailwindcss", "tsserver" }
+local servers = { "clangd", "metals", "tailwindcss", "tsserver" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -21,4 +21,44 @@ lspconfig.tailwindcss.setup {
   on_init = on_init,
   capabilities = capabilities,
   filetypes = { "html", "css", "rust" },
+}
+
+-- customization for rust_analyzer
+lspconfig.rust_analyzer.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  settings = {
+    ["rust-analyzer"] = {
+      assist = {
+        importMergeBehavior = "last",
+        importPrefix = "by_self",
+      },
+      cargo = {
+        allFeatures = true,
+        loadOutDirsFromCheck = true,
+        runBuildScripts = true,
+      },
+      checkOnSave = {
+        allFeatures = true,
+        command = "clippy",
+        extraArgs = { "--no-deps" },
+      },
+      inlayHints = {
+        enable = true,
+        typeHints = true,
+        parameterHints = true,
+        chainingHints = true,
+        maxLength = 100,
+      },
+      procMacro = {
+        enable = true,
+        ignored = {
+          ["async-trait"] = { "async_trait" },
+          ["napi-derive"] = { "napi" },
+          ["async-recursion"] = { "async_recursion" },
+        },
+      },
+    },
+  },
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -135,7 +135,6 @@ return {
       end, 0)
     end,
   },
-
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
@@ -143,5 +142,42 @@ return {
     config = function(_, opts)
       require("ibl").setup()
     end,
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>dx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>dX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>dS",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>dl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>dL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
   },
 }
