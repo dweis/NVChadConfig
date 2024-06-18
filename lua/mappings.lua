6c5cbe62626ef78c9f262cddf2ba4a731bb52486
Derrick Weis <derrick@derrickweis.com>
2024-06-17 23:33:33 -0700

    new DAP bindings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,8 +6,43 @@ local map = vim.keymap.set
 
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map("i", "jk", "<ESC>")
-map("n", "<leader>rd", "<cmd>DapContinue<CR>", { desc = "DAP Continue" })
-map("n", "<leader>rt", "<cmd>DapToggleBreakpoint<CR>", { desc = "DAP Toggle Breakpoint" })
-map("n", "<leader>rr", "<cmd>DapToggleRepl<CR>", { desc = "DAP Toggle Repl" })
+
+map("n", "<leader>dr", function()
+  require("dap").repl_open()
+end, { desc = "Debugger Toggle Repl" })
+
+map("n", "<leader>db", function()
+  require("dap").toggle_breakpoint()
+end, { desc = "Toggle Breakpoint" })
+
+map({ "n", "v" }, "<leader>dh", function()
+  require("dap.ui.widgets").hover()
+end, { desc = "Debugger UI Hover" })
+
+map("n", "<leader>df", function()
+  require("dap.ui.widgets").preview()
+end, { desc = "Debugger UI Preview" })
+
+map("n", "<leader>df", function()
+  local widgets = require "dap.ui.widgets"
+  widgets.centered_float(widgets.frames)
+end, { desc = "Debugger UI Widgets" })
+
+map("n", "<leader>dR", function()
+  require("dap").run_last()
+end, { desc = "Run Last" })
+
+map("n", "<leader>dc", function()
+  require("dap").continue()
+end, { desc = "Debug Continue" })
+map("n", "<leader>dv", function()
+  require("dap").step_over()
+end, { desc = "Step Over" })
+map("n", "<leader>d>", function()
+  require("dap").step_into()
+end, { desc = "Step In" })
+map("n", "<leader>d<", function()
+  require("dap").step_out()
+end, { desc = "Step Out" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
